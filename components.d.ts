ab01c5e05f7d5e73396289b6641bf54f161a9ba7
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2024-07-19 18:52:05 +0300

    Removed unused files

--- components.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "components/**/*" {
-    const content: any;
-    export default content;
-  }
-  
--- styles.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "styles/*" {
-    const content: any;
-    export default content;
-  }
-  
